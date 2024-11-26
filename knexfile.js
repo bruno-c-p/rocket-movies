6707be0ba47e8ffaeed8888b586fcf0a3b7106ed
bruno-c-p <bruno.cardozo.pereira@gmail.com>
2024-11-26 17:11:09 -0300

    chore: add knex production config

--- knexfile.js
+++ knexfile.js
@@ -23,4 +23,23 @@ export default {
       ),
     },
   },
+  production: {
+    client: "sqlite3",
+    connection: {
+      filename: path.resolve(__dirname, "src", "database", "database.db"),
+    },
+    pool: {
+      afterCreate: (conn, cb) => conn.run("PRAGMA foreign_keys = ON", cb),
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: path.resolve(
+        __dirname,
+        "src",
+        "database",
+        "knex",
+        "migrations"
+      ),
+    },
+  },
 };
